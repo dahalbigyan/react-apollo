c5e73b2146a538e3b284ce90d569980b01d36649
BigYan Dahal <bigyandahal@BigYans-MacBook-Pro.local>
2018-03-07 18:58:44 -0500

    More scaffolding files.

---
+++ src/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <ul>
+      <li>Bigyan Dahal</li>
+      <li>Liverpool FC</li>
+    </ul>
+  );
+}
+
+export default Contacts;
